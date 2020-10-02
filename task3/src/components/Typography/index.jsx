0240cbe8c8c1ac83b5324bd4b529ec20629dca93
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-02 11:18:24 +0300

    Added typography to storybooks

--- task3/src/components/Typography/index.jsx
+++ task3/src/components/Typography/index.jsx
@@ -1,36 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import './Typography.css';
 
-export const Typography = ({ tag, text }) => {
+export const Typography = ({ tag, text, className }) => {
     return (
-         React.createElement(tag, {color: 'red'}, text)
+         React.createElement(tag, {className: className}, text)
     );
 };
 
-// Button.propTypes = {
-//     /**
-//      * Is this the principal call to action on the page?
-//      */
-//     primary: PropTypes.bool,
-//     /**
-//      * What background color to use
-//      */
-//     backgroundColor: PropTypes.string,
-//     /**
-//      * How large should the button be?
-//      */
-//     size: PropTypes.oneOf(['small', 'medium', 'large']),
-//     /**
-//      * Button contents
-//      */
-//     label: PropTypes.string.isRequired,
-//     /**
-//      * Optional click handler
-//      */
-//     onClick: PropTypes.func,
-// };
-
 Typography.defaultProps = {
-    tag: 'h2',
     text: 'The quick brown fox jumps over the lazy dog.'
 };
