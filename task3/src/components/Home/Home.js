92674b339f6688708959d588ac04387976f8c148
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-12 16:30:15 +0300

    fix: fixed footer icons

--- task3/src/components/Home/Home.js
+++ task3/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import LetsGet from '../LetsGet/LetsGet';
-import Tools from '../Tools/Tools';
-import Users from '../Users/Users';
+// import Tools from '../Tools/Tools';
+// import Users from '../Users/Users';
+const Users = React.lazy(() => import('../Users/Users'));
+const Tools = React.lazy(() => import('../Tools/Tools'));
 
 export default function Home() {
   return (
