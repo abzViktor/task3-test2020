ed2c5190cbbe60fbc014dd483784c284d1c3e147
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-15 10:26:38 +0300

    dev: creating registration form, added validation for 3 fields

--- task3/src/components/Registration/components/Banner/RegistrationBanner.jsx
+++ task3/src/components/Registration/components/Banner/RegistrationBanner.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function RegistrationBanner() {
+  const { t } = useTranslation();
   return (
-    <div>Banner</div>
+    <div className="image-container">
+      <picture>
+        {/* <source srcSet="reg-banner/bg-3840-2x.webp 2x ,reg-banner/bg-3840.webp 1x" media="(min-width: 3840px)" type="image/webp" /> */}
+        {/* <source srcSet="reg-banner/bg-3840-2x.jpg 2x ,reg-banner/bg-3840.jpg 1x" media="(min-width: 3840px)" type="image/webp" /> */}
+
+        <source srcSet="reg-banner/bg-2560.webp 1x" media="(min-width: 2560px)" type="image/webp" />
+        <source srcSet="reg-banner/bg-2560.jpg 1x" media="(min-width: 2560px)" type="image/jpeg" />
+
+        {/* <source srcSet="reg-banner/bg-1920-2x.webp 2x ,reg-banner/bg-1920.webp 1x" media="(min-width: 1920px)" type="image/webp" /> */}
+        {/* <source srcSet="reg-banner/bg-1920-2x.jpg 2x ,reg-banner/bg-1920.jpg 1x" media="(min-width: 1920px)" type="image/jpeg" /> */}
+
+        {/* <source srcSet="reg-banner/bg-1440-2x.webp 2x ,reg-banner/bg-1440.webp 1x" media="(min-width: 1440px)" type="image/webp" /> */}
+        {/* <source srcSet="reg-banner/bg-1440-2x.jpg 2x ,reg-banner/bg-1440.jpg 1x" media="(min-width: 1440px)" type="image/jpeg" /> */}
+
+        <source srcSet="reg-banner/bg-1024.webp 1x" media="(min-width: 1024px)" type="image/webp" />
+        <source srcSet="reg-banner/bg-1024.jpg 1x" media="(min-width: 1024px)" type="image/jpeg" />
+
+        <source srcSet="reg-banner/bg-992.webp 1x" media="(min-width: 992px)" type="image/webp" />
+        <source srcSet="reg-banner/bg-992.jpg 1x" media="(min-width: 992px)" type="image/jpeg" />
+
+        <source srcSet="reg-banner/bg-768.webp 1x" media="(min-width: 700px)" type="image/webp" />
+        <source srcSet="reg-banner/bg-768.jpg 1x" media="(min-width: 700px)" type="image/jpeg" />
+
+        <source srcSet="reg-banner/bg-320.webp 1x" media="(min-width: 320px)" type="image/webp" />
+        <source srcSet="reg-banner/bg-320.jpg 1x" media="(min-width: 320px)" type="image/jpeg" />
+        <img src="banner/bg-1024.jpg" alt="Main banner of registration page" />
+      </picture>
+      <div className="banner-container">
+        <div className="banner-text container">
+          <div className="registration-text-block">
+            <h1 className="heading-2-desktop">
+              General requirements for the test task
+            </h1>
+            <div className="registration-banner-content">
+              <div className="paragraphs-banner">
+                <p className="paragraph-2">Users want to find answers to their questions quickly and data shows that people really care about how quickly their pages load. The Search team announced speed would be a ranking signal for desktop searches in 2010 and as of this month (July 2018), page speed will be a ranking factor for mobile searches too.</p>
+                <p className="paragraph-2">If you're a developer working on a site, now is a good time to evaluate your performance using our speed tools. Think about how performance affects the user experience of your pages and consider measuring a variety of real-world user-centric performance metrics.</p>
+                <p className="paragraph-2">Are you shipping too much JavaScript? Too many images? Images and JavaScript are the most significant contributors to the page weight that affect page load time based on data from HTTP Archive and the Chrome User Experience Report - our public dataset for key UX metrics as experienced by Chrome users under real-world conditions.</p>
+              </div>
+              <div>
+                <div className="image-container">
+                  <picture>
+                    <source srcSet="images/man-laptop-v1.svg" media="(min-width: 320px) and (min-width: 1024px)" />
+                    <source srcSet="images/man-laptop-v2.svg" media="(min-width: 500px)" />
+                    <img src="images/man-laptop-v1.svg" alt="Man from registration page" />
+                  </picture>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- task3/src/components/Registration/components/Form/RegistrationForm.jsx
+++ task3/src/components/Registration/components/Form/RegistrationForm.jsx
@@ -1,7 +1,199 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputMask from 'react-input-mask';
+import {
+  Formik, Field, Form, FieldArray,
+} from 'formik';
+import * as yup from 'yup';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import { FormControl } from '@material-ui/core';
+import { ArrowIcon } from '../../../Input/Input';
+import '../../../Input/Input.css';
 
 export default function RegistrationForm() {
+  // const [value, setValue] = React.useState(1);
+  // const handleChange = (event) => {
+  //   setValue(event.target.value);
+  // };
+  const initialValues = {
+    name: '',
+    email: '',
+    phone: '',
+    // position: 1,
+    // file: '',
+  };
+
+  const validateNumberMask = (value) => {
+    let error;
+    if (value === '+38(0__)___-__-__' || !value) {
+      error = '';
+    } else if (!value.match(/^\+38\(0\d{2}\)\d{3}-\d{2}-\d{2}$/)) {
+      error = 'Phone number should be in +38(0XX)XXX-XX-XX format!';
+    }
+    return error;
+  };
+
+  const validationSchema = yup.object({
+    name: yup.string()
+      .min(2, 'Name should be at least 2 characters!')
+      .max(60, 'Name should be less than 128 characters!')
+      .matches(/^[A-Za-z\s]+$/g, 'Name should contain only latin letters!'),
+    email: yup.string()
+      .max(254, 'Email should be less than 254 characters!')
+      .matches(/^[\w-]{1,64}?@[\w-]+\.[\w-]{2,}/, 'Invalid Email!'),
+  });
+  // const hiddenFileInput = React.useRef(null);
+  // const handleUpload = (ev) => {
+  //   ev.preventDefault();
+  //   hiddenFileInput.current.click();
+  // };
+
+  // const [fileValue, setFileValue] = React.useState('Upload your photo');
+  //
+  // const handleUp = (event) => {
+  //   if (event.target.value !== '') {
+  //     setFileValue(event.target.value.split('\\')[event.target.value.split('\\').length - 1]);
+  //   }
+  // };
+  //
+  // const handleSubmit = (ev) => {
+  //   ev.preventDefault();
+  //   console.log(ev.target.value);
+  // };
+
   return (
-    <div>Form</div>
+    <div className="form">
+      <div className="container">
+        <h1 className="heading-2-desktop">Register to get a work</h1>
+        <p className="paragraph-1">Attention! After successful registration and alert, update the list of users in the block from the top</p>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(data, { setSubmitting }) => {
+            setSubmitting(true);
+            console.log(data);
+            setSubmitting(false);
+          }}
+          validationSchema={validationSchema}
+        >
+          {({
+            values,
+            setFieldValue,
+            setFieldTouched,
+            dirty,
+            isValid,
+            errors,
+            touched,
+          }) => (
+            <Form>
+              <div className="firstFormRow">
+                <div>
+                  <Field
+                    required
+                    name="name"
+                    type="input"
+                    as={TextField}
+                    label="Name"
+                    placeholder="Your name"
+                    variant="outlined"
+                    helperText={touched.name ? errors.name : ''}
+                    error={touched.name && (!!errors.name)}
+                  />
+                </div>
+                <div>
+                  <Field
+                    required
+                    name="email"
+                    label="Email"
+                    placeholder="Your email"
+                    variant="outlined"
+                    as={TextField}
+                    helperText={touched.email ? errors.email : ''}
+                    error={touched.email && (!!errors.email)}
+                  />
+                </div>
+                <div>
+                  <Field
+                    name="phone"
+                    validate={validateNumberMask}
+                  >
+                    {() => (
+                      <InputMask
+                        value={values.phone}
+                        mask="+38(099)999-99-99"
+                        onChange={({ target }) => {
+                          setFieldValue('phone', target.value);
+                        }}
+                        onBlur={() => {
+                          setFieldTouched('phone', true);
+                        }}
+                      >
+                        {() => (
+                          <TextField
+                            variant="outlined"
+                            type="tel"
+                            required
+                            label="Phone"
+                            helperText={touched.phone ? errors.phone : ''}
+                            error={touched.phone && (!!errors.phone)}
+                          />
+                        )}
+                      </InputMask>
+                    )}
+                  </Field>
+                </div>
+              </div>
+              <div className="secondFormRow">
+
+                {/* <div className="select-component"> */}
+                {/*  <FieldArray name="position"> */}
+                {/*    {() => { */}
+
+                {/*    }} */}
+                {/*  </FieldArray> */}
+                {/*  <Select */}
+                {/*    name="position" */}
+                {/*    className="selectedItem formSelect" */}
+                {/*    variant="outlined" */}
+                {/*    value={values.position} */}
+                {/*    onChange={handleChange} */}
+                {/*    fullWidth */}
+                {/*    autoWidth */}
+                {/*    IconComponent={ArrowIcon} */}
+                {/*  > */}
+                {/*    <MenuItem value={1}>First item</MenuItem> */}
+                {/*    <MenuItem value={2}>Second item</MenuItem> */}
+                {/*    <MenuItem value={3}>Third item</MenuItem> */}
+                {/*  </Select> */}
+                {/* </div> */}
+                {/* <div> */}
+                {/*  <div className="fileFieldHolder"> */}
+                {/*    <TextField */}
+                {/*      inputProps={{ readOnly: 'readonly' }} */}
+                {/*      helperText="File format jpg  up to 5 MB, the minimum size of 70x70px" */}
+                {/*      onClick={handleUpload} */}
+                {/*      ref={hiddenFileInput} */}
+                {/*      value={fileValue} */}
+                {/*      variant="outlined" */}
+                {/*    /> */}
+                {/*    <button type="button" onClick={handleUpload} className="secondary">Upload</button> */}
+                {/*  </div> */}
+                {/*  <> */}
+                {/*    <input onChange={handleUp} type="file" name="file" ref={hiddenFileInput} style={{ display: 'none' }} /> */}
+                {/*  </> */}
+                {/* </div> */}
+              </div>
+              <div className="submit-holder">
+                <button disabled={!(isValid && values.name !== '' && values.email !== '' && values.phone !== '' && values.phone !== '+38(0__)___-__-__')} className="primary" type="submit">Submit</button>
+              </div>
+              <pre>{JSON.stringify(values, null, 2)}</pre>
+              <pre>{JSON.stringify(errors, null, 2)}</pre>
+              <pre>{JSON.stringify(dirty, null, 2)}</pre>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 }
