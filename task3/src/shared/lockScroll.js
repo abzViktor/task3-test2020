51e6801ccfb90d04e7a32f3aa86a5b8e3a4dec71
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-22 14:16:27 +0300

    fix: lazyload only route components

--- task3/src/shared/lockScroll.js
+++ task3/src/shared/lockScroll.js
@@ -1,13 +1,41 @@
 // const html = document.querySelector('html');
 // const body = document.querySelector('body');
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
+const keys = {
+  37: 1, 38: 1, 39: 1, 40: 1,
+};
 
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault) e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
 const lockScroll = {
   enable() {
-    body.classList.add('lock');
+    if (window.addEventListener) // older FF
+    {
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+    }
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove = preventDefault; // mobile
+    document.onkeydown = preventDefaultForScrollKeys;
   },
   disable() {
-    body.classList.remove('lock');
+    if (window.removeEventListener) {
+      window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    }
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
   },
 };
 
