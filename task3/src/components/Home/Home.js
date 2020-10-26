f02bce6abf3abbaf02b505c98dfed38da43e5c73
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-26 12:27:10 +0200

    fix: split main page another way

--- task3/src/components/Home/Home.js
+++ task3/src/components/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import Banner from '../Banner/Banner';
-import LetsGet from '../LetsGet/LetsGet';
-import Users from '../Users/Users';
-import Tools from '../Tools/Tools';
+// import Banner from '../Banner/Banner';
+// import LetsGet from '../LetsGet/LetsGet';
+// import Users from '../Users/Users';
+// import Tools from '../Tools/Tools';
+
+const Banner = React.lazy(() => import('../Banner/Banner'));
+const LetsGet = React.lazy(() => import('../LetsGet/LetsGet'));
+const Users = React.lazy(() => import('../Users/Users'));
+const Tools = React.lazy(() => import('../Tools/Tools'));
 
 export default function Home() {
   return (
