04f440906107c60bcf7730c346bb315a7365403a
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-26 11:18:06 +0200

    home page splitting code

--- task3/src/components/Home/Home.js
+++ task3/src/components/Home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import Banner from '../Banner/Banner';
-import LetsGet from '../LetsGet/LetsGet';
-import Users from '../Users/Users';
-import Tools from '../Tools/Tools';
+import loadableVisibility from 'react-loadable-visibility/loadable-components';
+
+// import Banner from '../Banner/Banner';
+// import LetsGet from '../LetsGet/LetsGet';
+// import Users from '../Users/Users';
+// import Tools from '../Tools/Tools';
+
+const Banner = loadableVisibility(() => import('../Banner/Banner'), {
+  fallback: <div />,
+});
+
+const LetsGet = loadableVisibility(() => import('../LetsGet/LetsGet'), {
+  fallback: <div />,
+});
+
+const Users = loadableVisibility(() => import('../Users/Users'), {
+  fallback: <div />,
+});
+
+const Tools = loadableVisibility(() => import('../Tools/Tools'), {
+  fallback: <div />,
+});
 
 export default function Home() {
   return (
