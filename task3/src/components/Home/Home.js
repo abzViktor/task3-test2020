99cb7d43fbef61bd4a98594fc18980eb13c34671
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-26 11:37:35 +0200

    fix: delete splitting main page

--- task3/src/components/Home/Home.js
+++ task3/src/components/Home/Home.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import loadableVisibility from 'react-loadable-visibility/loadable-components';
-
-// import Banner from '../Banner/Banner';
-// import LetsGet from '../LetsGet/LetsGet';
-// import Users from '../Users/Users';
-// import Tools from '../Tools/Tools';
-
-const Banner = loadableVisibility(() => import('../Banner/Banner'), {
-  fallback: <div />,
-});
-
-const LetsGet = loadableVisibility(() => import('../LetsGet/LetsGet'), {
-  fallback: <div />,
-});
-
-const Users = loadableVisibility(() => import('../Users/Users'), {
-  fallback: <div />,
-});
-
-const Tools = loadableVisibility(() => import('../Tools/Tools'), {
-  fallback: <div />,
-});
+import Banner from '../Banner/Banner';
+import LetsGet from '../LetsGet/LetsGet';
+import Users from '../Users/Users';
+import Tools from '../Tools/Tools';
 
 export default function Home() {
   return (
