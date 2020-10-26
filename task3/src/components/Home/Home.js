f97be4264a8f9d4fbad5f707fb711d6cc0c76b7d
Viktor Peredera <viktor.peredera@abz.agency>
2020-10-26 12:48:40 +0200

    fix

--- task3/src/components/Home/Home.js
+++ task3/src/components/Home/Home.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import Banner from '../Banner/Banner';
-// import LetsGet from '../LetsGet/LetsGet';
-// import Users from '../Users/Users';
-// import Tools from '../Tools/Tools';
-
-const Banner = React.lazy(() => import('../Banner/Banner'));
-const LetsGet = React.lazy(() => import('../LetsGet/LetsGet'));
-const Users = React.lazy(() => import('../Users/Users'));
-const Tools = React.lazy(() => import('../Tools/Tools'));
+import Banner from '../Banner/Banner';
+import LetsGet from '../LetsGet/LetsGet';
+import Users from '../Users/Users';
+import Tools from '../Tools/Tools';
 
 export default function Home() {
   return (
