0726ce3874d5f74b7105af5ee07b80efaf157ce2
Trainee_abz <trainee_abz@Trainee-abzs-MacBook-Pro.local>
2020-11-03 23:58:32 +0200

    fix: fixed sitemap

--- task3.1/ssr-task3/utils/generateSiteMap.js
+++ task3.1/ssr-task3/utils/generateSiteMap.js
@@ -11,9 +11,8 @@ async function generateSiteMap() {
         'posts/*.md'
     ])
 
-    const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+                        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
           ${pages
         .map(page => {
             const path = page
@@ -29,8 +28,7 @@ async function generateSiteMap() {
                   `
         })
         .join('')}
-      </urlset>
-  `
+      </urlset>`
 
     fs.writeFileSync('public/sitemap.xml', sitemap)
 }
