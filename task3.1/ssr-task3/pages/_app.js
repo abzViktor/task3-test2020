c3ab3689c19f2e018658c311faea1b7345e63549
Trainee_abz <trainee_abz@Trainee-abzs-MacBook-Pro.local>
2020-11-03 19:07:09 +0200

    fix: move GTA to _app inside head tag

--- task3.1/ssr-task3/pages/_app.js
+++ task3.1/ssr-task3/pages/_app.js
@@ -14,19 +14,16 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <script
-            dangerouslySetInnerHTML={{
+      <Head dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NDVNDGK');
+                })(window,document,'script','dataLayer','GTM-NDVNDGK')</script>;
                                     `,
             }}
-        />
-      </Head>
+      />
       <RootProvider>
         <Layout>
           <Component {...pageProps} />
