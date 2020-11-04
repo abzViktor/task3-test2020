2b2065b99fbdea3aece074f1519e449eeed9f566
Trainee_abz <trainee_abz@Trainee-abzs-MacBook-Pro.local>
2020-11-04 09:35:41 +0200

    dev: added translation for 404 page

--- task3.1/ssr-task3/pages/404.js
+++ task3.1/ssr-task3/pages/404.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from '../components/404/404.module.scss';
 import NotFoundImage from '../components/404/404-image';
+import {useTranslation} from "react-i18next";
 
 export default function NotFound() {
+    const {t} = useTranslation();
     return (
         <div className={styles.notFoundContainer}>
             <div className="container">
                 <div className={styles.notFoundContent}>
-                    <h1 className="heading-2-desktop">Page is not found</h1>
-                    <p className="paragraph-1">Definitely, something went wrong, we can't find the page you searched</p>
+                    <h1 className="heading-2-desktop">{t('NotFound.heading')}</h1>
+                    <p className="paragraph-1">{t('NotFound.subheading')}</p>
                     <div className={styles.image404}>
                         <NotFoundImage />
                     </div>
