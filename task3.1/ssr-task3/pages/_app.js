30e272aacf733c49a518653fd7ba882eddd24c5a
Viktor Peredera <viktor.peredera@abz.agency>
2020-11-11 10:38:43 +0200

    fix: added preconnect to fonts

--- task3.1/ssr-task3/pages/_app.js
+++ task3.1/ssr-task3/pages/_app.js
@@ -21,7 +21,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <RootProvider>
       <Head>
-        <link rel="preload stylesheet" href="fonts/fonts.css" as="style" crossOrigin />
         {/* <script dangerouslySetInnerHTML={{__html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NDVNDGK');"}}/> */}
       </Head>
       <Layout>
--- task3.1/ssr-task3/pages/_document.js
+++ task3.1/ssr-task3/pages/_document.js
@@ -8,7 +8,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@600&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
